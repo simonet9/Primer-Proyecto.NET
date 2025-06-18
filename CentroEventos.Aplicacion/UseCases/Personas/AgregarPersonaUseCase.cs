51d4195ca76ba915408bd2e0ab8610b0e109b9b9
simonet9 <simon.castillo255651@alumnos.info.unlp.edu.ar>
2025-06-17 23:41:37 -0300

    Validando DNI único al agregar nueva persona en el caso de uso

--- CentroEventos.Aplicacion/UseCases/Personas/AgregarPersonaUseCase.cs
+++ CentroEventos.Aplicacion/UseCases/Personas/AgregarPersonaUseCase.cs
@@ -1,5 +1,4 @@
 using CentroEventos.Aplicacion.Entities;
-using CentroEventos.Aplicacion.Enum;
 using CentroEventos.Aplicacion.Exceptions;
 using CentroEventos.Aplicacion.Interfaces;
 using CentroEventos.Aplicacion.Validators;
@@ -11,8 +10,8 @@ namespace CentroEventos.Aplicacion.UseCases.Personas
         public void Ejecutar(Persona persona)
         {
             ValidadorPersona.Validar(persona);
-            if (repo.BuscarPorId(persona.Id) != null)
-                throw new OperacionInvalidaException("Ya existe una persona con el mismo ID.");
+            if (repo.ExisteDNI(persona.Dni))
+                throw new DuplicadoException("Ya existe una persona con el mismo DNI.");
             repo.Agregar(persona);
             repo.GuardarCambios();
         }
